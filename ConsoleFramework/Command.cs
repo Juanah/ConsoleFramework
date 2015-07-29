f752255b4cc5277dc97b20e6c633ff0a86b781d2
jonas@juanah.de <jonas@juanah.de>
2015-07-29 11:14:51 +0200

    Command finished

--- ConsoleFramework/Command.cs
+++ ConsoleFramework/Command.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleFramework.Data;
 
 namespace ConsoleFramework
 {
@@ -13,5 +14,52 @@ namespace ConsoleFramework
     /// </summary>
     public class Command
     {
+        private string _name;
+        private CaseLevel _caseLevel;
+        private IList<Option> _options;
+        private IList<Parameter> _parameters;
+        private bool _hasOptions, _hasParamertes;
+        public string Name { get { return _name; } }
+        public CaseLevel CaseLevel { get { return _caseLevel; } }
+        public IList<Option> Options { get { return _options; } }
+        public IList<Parameter> Parameters { get { return _parameters; } }
+        public bool HasOptions { get { return _hasOptions; } }
+        public bool HasParameters { get { return _hasParamertes; } }
+
+        #region Constructor
+        public Command(string name, CaseLevel caseLevel)
+        {
+            _name = name;
+            _caseLevel = caseLevel;
+            _hasOptions = false;
+            _hasParamertes = false;
+        }
+
+        public Command(string name, CaseLevel caseLevel, IList<Option> options, IList<Parameter> parameters)
+        {
+            _name = name;
+            _caseLevel = caseLevel;
+            _options = options;
+            _parameters = parameters;
+            _hasOptions = true;
+            _hasParamertes = true;
+        }
+
+        public Command(string name, CaseLevel caseLevel, IList<Option> options)
+        {
+            _name = name;
+            _caseLevel = caseLevel;
+            _options = options;
+            _hasOptions = true;
+        }
+
+        public Command(string name, CaseLevel caseLevel, IList<Parameter> parameters)
+        {
+            _name = name;
+            _caseLevel = caseLevel;
+            _parameters = parameters;
+            _hasParamertes = true;
+        }
+        #endregion
     }
 }
